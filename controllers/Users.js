fcfdd3634b1cf9a20020c7a6763b3f93ccfe115f
Mordekarsi <oscarolivarezcarsi@gmail.com>
2023-05-01 14:21:41 -0600

    Inicio de metodos para crear cuentas

--- controllers/Users.js
+++ controllers/Users.js
@@ -1,39 +1,11 @@
 const {response} = require('express');
 const UsersDAO = require('../dao/UsersDAO');
-const usuariosGet = (req, res = response) => {
-    res.json({
-        msg:" api GET desde controlador"
-    });
+async function createUser(req, res){
+    const {username, password, email, rol, name, paternalSurname, maternalSurname, schoolId, educationalProgram} = req.body;
+    const user = {username, password, email, rol};
+    const academic = 'ACADEMIC';
+    const student = 'STUDENT';
+    if (rol == academic){
+        
+    }
 }
-const usuariosPost = (req, res = response) => {
-    //const body = req.body;
-    const{nombre, edad} = req.body;
-    res.json({
-        msg:" api POST desde controlador",
-        //body
-        nombre,
-        edad
-    });
-}
-const usuariosPut = (req, res = response) => {
-    res.json({
-        msg:" api PUT desde controlador"
-    });
-}
-const usuariosPatch = (req, res = response) => {
-    res.json({
-        msg:" api PATCH desde controlador"
-    });
-}
-const usuariosDelete = (req, res = response) => {
-    res.json({
-        msg:" api DELETE desde controlador"
-    });
-}
-module.exports = {
-    usuariosGet,
-    usuariosPost,
-    usuariosPut,
-    usuariosPatch,
-    usuariosDelete
-};
