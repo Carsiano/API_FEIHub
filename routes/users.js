4f56501a5025c1c8487eddad42e68ff137416b62
Oscar Ivan Olivares Carsi <99694349+Carsiano@users.noreply.github.com>
2023-06-09 22:42:13 -0600

    add methods in users

--- routes/users.js
+++ routes/users.js
@@ -10,7 +10,7 @@ const { validateJWT } = require('../middlewares/validationJWT');
 const router = express.Router();
 
 router.get('/:username', userByUsernameGet); 
-router.get('/findUsers/:username', similarUserByUsernameGet); 
+router.get('/findUsers/:username',[validateJWT], similarUserByUsernameGet); 
 router.post('/', createUserPost);
 router.put('/:username',[validateJWT], userUpdatePut);
 router.patch('/:username',[validateJWT], userUpdateProfilePhotoPatch);
